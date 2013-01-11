0130eaf57a0f5e9469cabf6a7ea627fee708e305
Guy Zana <guy@cloudius-systems.com>
2013-01-12 01:50:33 +0200

    Disabling turned-on-by-default gcc flags in Ubuntu

--- config.sh
+++ config.sh
@@ -1,2 +1,3 @@
     ./configure --enable-static --disable-shared \
-               --enable-cxx-exceptions --disable-block-signals
+               --enable-cxx-exceptions --disable-block-signals \
+               CFLAGS="-U_FORTIFY_SOURCE -fno-stack-protector"
